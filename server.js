267773a2f7978ceaad4c0b5e1cd23145055c1f09
David Baker <drbaker2@gmail.com>
2016-10-15 20:21:07 -0400

    process.env.PORT || 3000

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(3000, () => {
-  console.log('server is running at port 3000')
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server is running at port ${process.env.PORT || 3000}`)
 })
 
 app.get('/', function(req, res) {
