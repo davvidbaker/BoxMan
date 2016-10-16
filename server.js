fe09a70f76b1d07c38043ae48ab9fbbc6f2d01d3
David Baker <drbaker2@gmail.com>
2016-10-15 20:14:16 -0400

    had forgotten to listen in the express server...fml

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'))
 
+app.listen(3000, () => {
+  console.log('server is running at port 3000')
+})
 
 app.get('/', function(req, res) {
   console.log(req);
